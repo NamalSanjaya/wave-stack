e5fd66ee639b870d1dbc305c081e7a133153bb52
NamalSanjaya <namalsanjaya23@gmail.com>
2023-05-09 08:24:45 +0530

    Add method to write to a tun interface

--- include/network.h
+++ include/network.h
@@ -1,8 +1,10 @@
 #ifndef _WAVE_NETWORK_H
 #define _WAVE_NETWORK_H
 
-// what if multiple network devices are availble.
-char *get_network_device();
+#include "pdu_buf.h"
+
+char *get_network_device();           // what if multiple network devices are availble.
 uint8_t *get_mac_addr(char *device);
+void write_tun(wave_pdu *pdu);
 
 #endif /* _WAVE_NETWORK_H */
