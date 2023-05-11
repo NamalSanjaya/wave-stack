69b0bf09cb8982b96902308aeaded0bb40fa535b
NamalSanjaya <namalsanjaya23@gmail.com>
2023-05-11 11:27:07 +0530

    Modify WSM request function logic

--- include/1609_3/wsmp.h
+++ include/1609_3/wsmp.h
@@ -29,7 +29,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#include "../../include/pdu_buf.h"
 #include "../../include/1609_3/wme.h"
 
 #define WSMP_EID_TX_POWER_USED_80211   4
@@ -290,7 +289,7 @@ struct wsmp_wsm *create_wsmp_metadata(uint8_t subtype, uint8_t tpid, uint8_t inf
      int8_t tx_pow, uint32_t psid, uint16_t len, uint8_t *data);
 struct wsmp_iex *create_n_iex(uint8_t chan_id, uint8_t data_rate, int8_t tx_pow);
 enum confirm_result_code wsm_waveshortmsg_req(uint8_t chan_id, enum time_slot timeslot, uint8_t data_rate, int8_t tx_power, uint8_t channel_load, 
-     uint8_t info_elem_indicator, uint8_t prority, uint64_t wsm_expire_time, uint16_t len, uint8_t *data, uint8_t *peer_mac_addr, uint32_t psid, wave_pdu *pdu);
+     uint8_t info_elem_indicator, uint8_t prority, uint64_t wsm_expire_time, uint16_t len, uint8_t *data, uint8_t *peer_mac_addr, uint32_t psid);
 
 /* int wsmp_ioctl(struct socket *sock, unsigned int cmd, unsigned long arg); */
 /* int wsmp_send(struct sk_buff *skb, int loop); */
