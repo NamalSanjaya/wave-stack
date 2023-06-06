dfa6f8a99633f7da3b880a977bc18c1c6b3aa15c
NamalSanjaya <namalsanjaya23@gmail.com>
2023-06-06 09:05:45 +0530

    Add error info method

--- include/fmt_error.h
+++ include/fmt_error.h
@@ -5,8 +5,10 @@
 #define WAVE_WARN "Warn"
 #define WAVE_DEBUG "Debug"
 #define WAVE_PANIC "Panic"
+#define WAVE_INFO "Info"
 
 void fmt_error(char *level, char *reason);
 void fmt_panic(char *reason);
+void fmt_info(char *reason);
 
 #endif /* _FMT_ERROR_H */
--- src/fmt_error.c
+++ src/fmt_error.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>
 #include "../include/fmt_error.h"
 
-extern int errno;
 
 // TODO: How to carry addtional data like err_code etc.
 void fmt_error(char *level, char *reason) {
-    fprintf(stderr, "%s : %s, %s\n", level, reason, strerror(errno));
+    fprintf(stderr, "%s : %s\n", level, reason);
 }
 
 void fmt_panic(char *reason) {
     fmt_error(WAVE_PANIC, reason);
     exit(EXIT_FAILURE);
+}
+
+void fmt_info(char *reason){
+    fprintf(stdout, "%s : %s\n", WAVE_INFO, reason);
 }
